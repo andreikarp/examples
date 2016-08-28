f5a54c8573c5413f1f7209b8ac855f960c18566e
andreikarp <andreikar@live.com>
2016-08-28 20:28:31 +0300

    Added project to solution

---
+++ hellocore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hellocore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, from .NET Core!");
+        }
+    }
+}
