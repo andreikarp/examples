192863590cd8c6627ba166264338417d68cb5fb4
andreikarp <andreikar@live.com>
2016-08-28 20:35:03 +0300

    Added standalone console app project.

---
+++ helloconsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, from Console App!");
+        }
+    }
+}
