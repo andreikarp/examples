59225f2f8ed645730b7bb916a3b43301d61cfbf3
andreikarp <andreikar@live.com>
2016-08-28 21:14:26 +0300

    ok

--- hellocore/Program.cs
+++ hellocore/Program.cs
@@ -6,7 +6,7 @@ namespace hellocore
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, from .NET Core!");
+            Console.WriteLine("Hello, from .NET Core!!!");
         }
     }
 }
