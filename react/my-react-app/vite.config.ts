b4b9e0640d8f77ed4804531608c4ca89d4de3d41
Andrei Karpenko <andreikar@live.com>
2025-03-04 08:16:00 +0300

    update configs

--- react/my-react-app/vite.config.ts
+++ react/my-react-app/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import checker from 'vite-plugin-checker'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), checker({
+    overlay: {
+      initialIsOpen: false
+    },
+    terminal: true,
+    enableBuild: false,
+    typescript: true,
+    eslint: {
+      useFlatConfig: true,
+      lintCommand: 'eslint "./src/**/*.{ts,tsx}"'
+    }
+  })],
 })
